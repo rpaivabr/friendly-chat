2ceadf2cead1000aca175b9b37dd0b40bdf1d061
Rafael Paiva <r.paivabr@gmail.com>
2025-04-12 19:15:37 -0300

    fix last

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,8 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { provideMessaging } from '@angular/fire/messaging';
-import { getMessaging } from 'firebase/messaging/sw';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+// import { getMessaging } from 'firebase/messaging/sw';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { environment } from '../environments/environment';
 
