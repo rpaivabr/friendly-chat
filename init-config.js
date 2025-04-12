6b10b7f3863ff5a519f06adf55bd840bc1d9cf48
Rafael Paiva <r.paivabr@gmail.com>
2025-04-12 20:00:07 -0300

    fix sw double message

--- init-config.js
+++ init-config.js
@@ -24,40 +24,40 @@ fs.writeFile(envTargetPath, envFile, (err) => {
     }
 });
 
-// const swFile = `importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
-// importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
+const swFile = `importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
-// const firebaseConfig =  { 
-//   projectId: "${process.env.FIREBASE_PROJECT_ID}", 
-//   appId: "${process.env.FIREBASE_APP_ID}", 
-//   storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}", 
-//   apiKey: "${process.env.FIREBASE_API_KEY}", 
-//   authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}", 
-//   messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}" 
-// };
-// firebase.initializeApp(firebaseConfig);
+const firebaseConfig =  { 
+  projectId: "${process.env.FIREBASE_PROJECT_ID}", 
+  appId: "${process.env.FIREBASE_APP_ID}", 
+  storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}", 
+  apiKey: "${process.env.FIREBASE_API_KEY}", 
+  authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}", 
+  messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}" 
+};
+firebase.initializeApp(firebaseConfig);
 
-// if (firebase.messaging.isSupported()) {
-//   const messaging = firebase.messaging();
+if (firebase.messaging.isSupported()) {
+  const messaging = firebase.messaging();
   
-//   messaging.onBackgroundMessage((payload) => {
-//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//     const notificationTitle = payload.notification.title;
-//     const notificationOptions = {
-//       body: payload.notification.body,
-//       icon: payload.notification.image,
-//     };
+  // messaging.onBackgroundMessage((payload) => {
+  //   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  //   const notificationTitle = payload.notification.title;
+  //   const notificationOptions = {
+  //     body: payload.notification.body,
+  //     icon: payload.notification.image,
+  //   };
   
-//     self.registration.showNotification(notificationTitle, notificationOptions);
-//   });
-// }`;
-// const swTargetPath = path.join(__dirname, './public/firebase-messaging-sw.js');
-// fs.writeFile(swTargetPath, swFile, (err) => {
-//     if (err) {
-//         console.error(err);
-//         throw err;
-//     } else {
-//         console.log(successColor, `${checkSign} Successfully generated public/firebase-messaging-sw.js`);
-//     }
-// });
+  //   self.registration.showNotification(notificationTitle, notificationOptions);
+  // });
+}`;
+const swTargetPath = path.join(__dirname, './public/firebase-messaging-sw.js');
+fs.writeFile(swTargetPath, swFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated public/firebase-messaging-sw.js`);
+    }
+});
 
